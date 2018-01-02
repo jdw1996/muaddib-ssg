888e53a3c0ce240da565a9e05cd43b4ddbbb3c03
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-01-01 23:14:49 -0500

    Created main file

---
+++ muaddib.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+import bs4
+from css_html_js_minify import css_minify, html_minify, js_minify
+import markdown as md
+
+# Prepare individual pages.
+
+# Organize pages into site.
+
+# Compile the site.
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
